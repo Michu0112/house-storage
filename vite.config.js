475c218f8671395c133f8fb7eacb9a652c944a66
Michał Pachuc <michal.pachuc@sunzinet.com>
2024-05-04 15:35:00 +0200

    feat: change pwa icon

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,14 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), vuetify(), VitePWA({ registerType: 'autoUpdate' })],
+    plugins: [
+        vue(),
+        vuetify(),
+        VitePWA({
+            includeAssets: ['favicon.ico', 'img/icons/apple-touch-icon.png', 'mask-icon.svg'],
+            registerType: 'autoUpdate'
+        })
+    ],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
