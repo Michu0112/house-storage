8c2ba3c956e1a7d7bb8306c367fc9fb3507fbe15
Michał Pachuc <michal.pachuc@sunzinet.com>
2024-05-04 15:27:17 +0200

    feat: add Vite PWA

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { fileURLToPath, URL } from 'node:url';
 import vuetify from 'vite-plugin-vuetify';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), vuetify()],
+    plugins: [vue(), vuetify(), VitePWA({ registerType: 'autoUpdate' })],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
